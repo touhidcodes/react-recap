1fe5509e7a7a9bf56c069fafe15302f8142f5f77
touhidcodes <touhidcodes@gmail.com>
2022-12-07 23:40:21 +0600

    react recap

--- src/App.js
+++ src/App.js
@@ -2,27 +2,28 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const name = ["Jasim", "Kabir", "Faltu", "Maltu"];
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
-      <User></User>
-      <User></User>
-      <User></User>
-      <User></User>
+      <User name={name[0]}></User>
+      <User name={name[1]}></User>
+      <User name={name[2]}></User>
+      <User name={name[3]}></User>
     </div>
   );
 }
 
-function User() {
+function User(props) {
   const style = {
     border: "2px solid red",
     borderRadius: "5px",
   };
   return (
     <div style={style}>
-      <h1>My name is:</h1>
+      <h1>My name is: {props.name}</h1>
       <p>My Occupation is"</p>
     </div>
   );
