1183b485a85e060c36187d90bda206425e7c982c
touhidcodes <touhidcodes@gmail.com>
2022-12-07 22:16:57 +0600

    react recap

--- src/App.js
+++ src/App.js
@@ -7,8 +7,24 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
+      <User></User>
+      <User></User>
+      <User></User>
+      <User></User>
     </div>
   );
 }
 
+function User() {
+  const style = {
+    border: "2px solid red",
+    borderRadius: "5px",
+  };
+  return (
+    <div style={style}>
+      <h1>My name is:</h1>
+      <p>My Occupation is"</p>
+    </div>
+  );
+}
 export default App;
